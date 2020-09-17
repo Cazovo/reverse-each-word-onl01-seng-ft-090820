abf58d5476b3914d95fa313c732c467877bc5a71
conner fortner <fortner.conner@yahoo.com>
2020-09-17 02:59:03 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,27 @@
+def reverse_each_word()
+    original_array = string.split(" ")
+    return_array = []
+    original_array.each do |string|
+      return_array << string.reverse
+    end
+    return_array.join(" ")
+end
+
+
+def reverse_each_word(string)
+    array = string.split(" ")
+    new_array = []
+    array.collect do |string|
+      new_array << string.reverse
+    end
+    new_array.join(" ")
+    
+end
+
+  
+      
+      
+      
+      
+      
+    
